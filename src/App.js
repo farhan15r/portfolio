1bb0051535dccdc87902cf32d52f8d7fe2995c85
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-07-24 22:30:30 +0700

    about me section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import AboutMeSection from "./components/AboutMeSection";
 import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
+      <AboutMeSection />
     </div>
   );
 }
